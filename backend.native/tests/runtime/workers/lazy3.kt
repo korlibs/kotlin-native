korlibs/kotlin-native

commit c6982fa0013fdab31c23764d9da3f6469f15e160

Fix lazy in presense of atomic references. (#3862)

runtime/src/main/kotlin/kotlin/native/concurrent/Lazy.kt
@@ -8,53 +8,49 @@ package kotlin.native.concurrent
 import kotlin.native.internal.Frozen
 import kotlin.native.internal.NoReorderFields
 
-@SymbolName("Konan_ensureAcyclicAndSet")
-private external fun ensureAcyclicAndSet(where: Any, index: Int, what: Any?): Boolean
-
-@SymbolName("ReadHeapRefNoLock")
-internal external fun readHeapRefNoLock(where: Any, index: Int): Any?
-
-@NoReorderFields
 internal class FreezeAwareLazyImpl<out T>(initializer: () -> T) : Lazy<T> {
-    // IMPORTANT: due to simplified ensureAcyclicAndSet() semantics fields here must be ordered like this,
-    // as an ordinal is used to refer a field.
-    private var value_: Any? = UNINITIALIZED
-    private var initializer_: (() -> T)? = initializer
+    private val value_ = FreezableAtomicReference<Any?>(UNINITIALIZED)
+    private val initializer_ = FreezableAtomicReference<(() -> T)?>(initializer)
     private val lock_ = Lock()
 
     override val value: T
         get() {
             if (isFrozen) {
                 locked(lock_) {
-                    // Lock is already taken above.
-                    var result = readHeapRefNoLock(this, 0)
+                    var result = value_.value
                     if (result !== UNINITIALIZED) {
-                        assert(result !== INITIALIZING)
+                        if (result === INITIALIZING) {
+                            throw IllegalStateException("Recursive lazy computation")
+                        }
                         @Suppress("UNCHECKED_CAST")
                         return result as T
                     }
                     // Set value_ to INITIALIZING.
-                    ensureAcyclicAndSet(this, 0, INITIALIZING)
-                    result = initializer_!!().freeze()
-                    // Set value_.
-                    if (!ensureAcyclicAndSet(this, 0, result)) {
-                        throw InvalidMutabilityException("Setting cyclic data via lazy in $this: $result")
-                    }
-                    // Do not clear initializer_ reference, as it may break freezing invariants and zero out
-                    // still valid object. It seems to be safe only in case when `this` is not reachable from
-                    // initializer.
+                    value_.value = INITIALIZING
+                    result = initializer_.value!!().freeze()
+                    // Set value_ to actual one.
+                    value_.value = result
+                    // Clear initializer.
+                    initializer_.value = null
+
                     @Suppress("UNCHECKED_CAST")
                     return result as T
                 }
             } else {
-                var result: Any? = value_
-                if (result === UNINITIALIZED) {
-                    result = initializer_!!()
-                    if (isFrozen)
-                        throw InvalidMutabilityException("$this got frozen during lazy evaluation" )
-                    value_ = result
-                    initializer_ = null
+                var result = value_.value
+                if (result !== UNINITIALIZED) {
+                    if (result === INITIALIZING) {
+                        throw IllegalStateException("Recursive lazy computation")
+                    }
+                    @Suppress("UNCHECKED_CAST")
+                    return result as T
                 }
+                value_.value = INITIALIZING
+                result = initializer_.value!!()
+                if (isFrozen)
+                    throw InvalidMutabilityException("$this got frozen during lazy evaluation" )
+                value_.value = result
+                initializer_.value = null
                 @Suppress("UNCHECKED_CAST")
                 return result as T
             }
@@ -63,10 +59,10 @@ internal class FreezeAwareLazyImpl<out T>(initializer: () -> T) : Lazy<T> {
     /**
      * This operation on shared objects may return value which is no longer reflect the current state of lazy.
      */
-    override fun isInitialized(): Boolean = (value_ !== UNINITIALIZED) && (value_ !== INITIALIZING)
+    override fun isInitialized(): Boolean = (value_.value !== UNINITIALIZED) && (value_.value !== INITIALIZING)
 
     override fun toString(): String = if (isInitialized())
-        value.toString() else "Lazy value not initialized yet."
+        value.toString() else "Lazy value not initialized yet"
 }
 
 internal object UNINITIALIZED {

backend.native/tests/runtime/workers/lazy1.kt
@@ -13,10 +13,43 @@ class Leak {
     val leak by lazy { this }
 }
 
-@Test fun runTest() {
+class SelfReference {
+    val x = 17
+    val self by lazy { this }
+    val recursion: Int by lazy {
+        if (x < 17) 42 else recursion
+    }
+    val freezer: Int by lazy {
+        freeze()
+        42
+    }
+}
+
+
+
+@Test fun runTest1() {
+    assertFailsWith<IllegalStateException> {
+        println(SelfReference().recursion)
+    }
+    assertFailsWith<IllegalStateException> {
+        println(SelfReference().freeze().recursion)
+    }
+}
+
+// Disabled, as requires cyclic collector.
+// @Test
+fun runTest2() {
+    var sum = 0
+    for (i in 1 .. 100) {
+        val self = SelfReference().freeze()
+        assertEquals(self, self.self)
+        sum += self.self.hashCode()
+    }
+}
+
+@Test fun runTest3() {
     assertFailsWith<InvalidMutabilityException> {
-        for (i in 1..100)
-            Leak().freeze().leak
+        println(SelfReference().freezer)
     }
     println("OK")
 }

runtime/src/main/cpp/Memory.cpp
@@ -2513,49 +2513,6 @@ void ensureNeverFrozen(ObjHeader* object) {
    object->meta_object()->flags_ |= MF_NEVER_FROZEN;
 }
 
-// TODO: incorrect, use 3-color scheme.
-KBoolean ensureAcyclicAndSet(ObjHeader* where, KInt index, ObjHeader* what) {
-    RuntimeAssert(where->container() != nullptr && where->container()->frozen(), "Must be used on frozen objects only");
-    RuntimeAssert(what == nullptr || isPermanentOrFrozen(what),
-        "Must be used with an immutable value");
-    if (what != nullptr) {
-        // Now we check that `where` is not reachable from `what`.
-        // As we cannot modify objects while traversing, instead we remember all seen objects in a set.
-        KStdUnorderedSet<ContainerHeader*> seen;
-        KStdDeque<ContainerHeader*> queue;
-        if (what->container() != nullptr)
-            queue.push_back(what->container());
-        bool acyclic = true;
-        while (!queue.empty() && acyclic) {
-            ContainerHeader* current = queue.front();
-            queue.pop_front();
-            seen.insert(current);
-            if (isAggregatingFrozenContainer(current)) {
-                ContainerHeader** subContainer = reinterpret_cast<ContainerHeader**>(current + 1);
-                for (int i = 0; i < current->objectCount(); ++i) {
-                    if (seen.count(*subContainer) == 0)
-                        queue.push_back(*subContainer++);
-                }
-            } else {
-              traverseContainerReferredObjects(current, [where, &queue, &acyclic, &seen](ObjHeader* obj) {
-                if (obj == where) {
-                    acyclic = false;
-                } else {
-                    auto* objContainer = obj->container();
-                    if (objContainer != nullptr && seen.count(objContainer) == 0)
-                        queue.push_back(objContainer);
-                }
-              });
-            }
-          }
-        if (!acyclic) return false;
-    }
-    UpdateHeapRef(reinterpret_cast<ObjHeader**>(
-            reinterpret_cast<uintptr_t>(where) + where->type_info()->objOffsets_[index]), what);
-    // Fence on updated location?
-    return true;
-}
-
 void shareAny(ObjHeader* obj) {
   auto* container = obj->container();
   if (isShareable(container)) return;
@@ -3101,10 +3058,6 @@ void MutationCheck(ObjHeader* obj) {
 
 void EnsureNeverFrozen(ObjHeader* object) {
   ensureNeverFrozen(object);
-}
-
-KBoolean Konan_ensureAcyclicAndSet(ObjHeader* where, KInt index, ObjHeader* what) {
-  return ensureAcyclicAndSet(where, index, what);
 }
 
 void Kotlin_Any_share(ObjHeader* obj) {

runtime/src/main/kotlin/kotlin/native/concurrent/MutableData.kt
@@ -14,6 +14,10 @@ external private fun Any.share()
 @SymbolName("Kotlin_CPointer_CopyMemory")
 external private fun CopyMemory(to: COpaquePointer?, from: COpaquePointer?, count: Int)
 
+@SymbolName("ReadHeapRefNoLock")
+internal external fun readHeapRefNoLock(where: Any, index: Int): Any?
+
+
 /**
  * Mutable concurrently accessible data buffer. Could be accessed from several workers simulteniously.
  */

commit d0a25c5908b6fe404668f1c9a73a0acd86e453ba

Fix `lazy {}` memory leak regression caused by #3862

runtime/src/main/kotlin/kotlin/native/concurrent/Lazy.kt
@@ -9,7 +9,7 @@ import kotlin.native.internal.Frozen
 
 internal class FreezeAwareLazyImpl<out T>(initializer: () -> T) : Lazy<T> {
     private val value_ = FreezableAtomicReference<Any?>(UNINITIALIZED)
-    private val initializer_ = FreezableAtomicReference<(() -> T)?>(initializer)
+    private var initializer_: (() -> T)? = initializer
     private val lock_ = Lock()
 
     private fun getOrInit(doFreeze: Boolean): T {
@@ -25,20 +25,22 @@ internal class FreezeAwareLazyImpl<out T>(initializer: () -> T) : Lazy<T> {
         // Set value_ to INITIALIZING.
         value_.value = INITIALIZING
         try {
-            result = initializer_.value!!()
+            result = initializer_!!()
             if (doFreeze) result.freeze()
         } catch (throwable: Throwable) {
             value_.value = UNINITIALIZED
             throw throwable
         }
-        if (!doFreeze && this.isFrozen) {
-            value_.value = UNINITIALIZED
-            throw InvalidMutabilityException("Frozen during lazy computation")
+        if (!doFreeze) {
+            if (this.isFrozen) {
+                value_.value = UNINITIALIZED
+                throw InvalidMutabilityException("Frozen during lazy computation")
+            }
+            // Clear initializer.
+            initializer_ = null
         }
         // Set value_ to actual one.
         value_.value = result
-        // Clear initializer.
-        initializer_.value = null
         @Suppress("UNCHECKED_CAST")
         return result as T
     }

backend.native/tests/build.gradle
@@ -1027,6 +1027,10 @@ standaloneTest("lazy2") {
     source = "runtime/workers/lazy2.kt"
 }
 
+standaloneTest("lazy3") {
+    source = "runtime/workers/lazy3.kt"
+}
+
 task enumIdentity(type: KonanLocalTest) {
     enabled = (project.testTarget != 'wasm32') // Workers need pthreads.
     goldValue = "true\n"

backend.native/tests/runtime/workers/lazy3.kt
@@ -1,0 +1,54 @@
+import kotlin.native.concurrent.*
+import kotlin.native.ref.*
+import kotlin.test.*
+
+fun main() {
+    test1()
+    test2()
+}
+
+fun test1() {
+    ensureGetsCollectedFrozenAndNotFrozen { LazyCapturesThis() }
+    ensureGetsCollectedFrozenAndNotFrozen {
+        val l = LazyCapturesThis()
+        l.bar
+        l
+    }
+    ensureGetsCollected {
+        val l = LazyCapturesThis().freeze()
+        l.bar
+        l
+    }
+}
+
+class LazyCapturesThis {
+    fun foo() = 42
+    val bar by lazy { foo() }
+}
+
+fun test2() {
+    ensureGetsCollectedFrozenAndNotFrozen { Throwable() }
+    ensureGetsCollectedFrozenAndNotFrozen {
+        val throwable = Throwable()
+        throwable.getStackTrace()
+        throwable
+    }
+    ensureGetsCollected {
+        val throwable = Throwable().freeze()
+        throwable.getStackTrace()
+        throwable
+    }
+}
+
+fun ensureGetsCollectedFrozenAndNotFrozen(create: () -> Any) {
+    ensureGetsCollected { create().freeze() }
+    ensureGetsCollected(create)
+}
+
+fun ensureGetsCollected(create: () -> Any) {
+    val ref = makeWeakRef(create)
+    kotlin.native.internal.GC.collect()
+    assertNull(ref.get())
+}
+
+fun makeWeakRef(create: () -> Any) = WeakReference(create())